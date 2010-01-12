07069c888eb708fcf97f6c6bfcfe852e72003411
nick <nick@uvilla.atemoya.net>
2010-01-11 23:36:09 -0500

    Renaming to Lingua::ART::Numbers and preparing for Lojban (jbo).

---
+++ lib/Lingua/JBO/Numbers.pm
@@ -0,0 +1,121 @@
+package Lingua::EO::Numbers;
+
+use 5.010;
+use strict;
+use warnings;
+use Regexp::Common;
+use Perl6::Junction qw( all );
+
+require Exporter;
+our @ISA = qw( Exporter );
+our @EXPORT = qw( num2eo );
+
+our $VERSION = 0.01;
+
+my @names1 = qw< nul unu du tri kvar kvin ses sep ok naŭ >;
+my @names2 = qw< dek cent mil >;
+my %words = (
+    ',' => 'komo',
+    '-' => 'negativa',
+    '+' => 'positiva',
+    inf => 'senfineco',
+    NaN => 'ne nombro',
+);
+
+sub num2eo {
+    my ($number) = @_;
+    my @names;
+
+    given ($number) {
+        when ($_ eq 'NaN') {
+            push @names, $words{NaN};
+        }
+        when (m/^ (?<sign> [-+] )? inf $/ixms) {
+            push @names, $+{sign} ? $words{ $+{sign} } : (), $words{inf};
+        }
+        when (m/^ $RE{num}{real}{-radix=>'[,.]'}{-keep} $/xms) {
+            my $sign = $2;
+            my $int  = $4;
+            my $frac = $6;
+
+            my @digits = split //, $int // q{};
+
+            # numbers >= a million not currently supported
+            return if @digits > 6;
+
+            DIGIT:
+            for my $i (1..@digits) {
+                my $digit = $digits[-$i];
+                my $name  = $names1[$digit];
+
+                # skip 0 unless it is the entire number
+                next DIGIT
+                    if !$digit && @digits != 1 && !($i == 4 && @digits > 4);
+
+                unshift(
+                    @names,
+                    $i == 1 ? $name : (
+                        $digit && (
+                            $digit != 1 || $i == 4 && @digits > 4
+                        ) ? $name . ( $i == 4 ? q{ } : q{} ) : q{}
+                    ) . $names2[ abs($i) - ($i < 5 ? 2 : 5) ]
+                );
+            }
+
+            if ( defined $frac && $frac ne q{} ) {
+                push @names, $words{','}, map { $names1[$_] } split //, $frac;
+            }
+
+            unshift @names, $words{$sign} || ();
+        }
+        default { return }
+    }
+
+    return join q{ }, @names;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Lingua::EO::Numbers - Convert numbers to Esperanto words
+
+=head1 SYNOPSIS
+
+  use Lingua::EO::Numbers
+
+  say 'Vi havas ', num2eo(int rand 1000), ' pomojn';
+
+=head1 DESCRIPTION
+
+The Lingua::EO::Numbers module provides one function, C<num2eo>, which
+converts numbers to words in Esperanto.
+
+=over 4
+
+=item num2eo
+X<num2eo>
+
+The C<num2eo> function ...
+
+=back
+
+=head1 SEE ALSO
+
+L<Lingua::EN::Numbers>, L<Lingua::Any::Numbers>
+
+=head1 AUTHOR
+
+Nick Patch, E<lt>patch@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself, either Perl version 5.10.0 or, at your option,
+any later version of Perl 5 you may have available.
+
+=cut
---
+++ t/jbo.t
@@ -0,0 +1,79 @@
+use 5.010;
+use strict;
+use warnings;
+use Test::More tests => 51;
+
+BEGIN { use_ok 'Lingua::EO::Numbers' }
+
+my @tests = (
+    [     -9 => 'negativa naŭ'    ],
+    [      0 => 'nul'             ],
+    [      9 => 'naŭ'             ],
+    [      9 => 'naŭ'             ],
+    [     10 => 'dek'             ],
+    [     90 => 'naŭdek'          ],
+    [     99 => 'naŭdek naŭ'      ],
+    [    100 => 'cent'            ],
+    [    109 => 'cent naŭ'        ],
+    [    110 => 'cent dek'        ],
+    [    190 => 'cent naŭdek'     ],
+    [    900 => 'naŭcent'         ],
+    [   1000 => 'mil'             ],
+    [   9000 => 'naŭ mil'         ],
+    [  10000 => 'dek mil'         ],
+    [  11000 => 'dek unu mil'     ],
+    [  19000 => 'dek naŭ mil'     ],
+    [  90000 => 'naŭdek mil'      ],
+    [ 100000 => 'cent mil'        ],
+    [ 110000 => 'cent dek mil'    ],
+    [ 190000 => 'cent naŭdek mil' ],
+    [ 900000 => 'naŭcent mil'     ],
+    [ 999999 => 'naŭcent naŭdek naŭ mil naŭcent naŭdek naŭ' ],
+
+    [ -9.0   => 'negativa naŭ'          ],
+    [ -0.9   => 'negativa nul komo naŭ' ],
+    [  0.0   => 'nul'                   ],
+    [  0.9   => 'nul komo naŭ'          ],
+    [  0.09  => 'nul komo nul naŭ'      ],
+    [  0.009 => 'nul komo nul nul naŭ'  ],
+    [  0.99  => 'nul komo naŭ naŭ'      ],
+    [  9.0   => 'naŭ'                   ],
+    [  9.9   => 'naŭ komo naŭ'          ],
+
+    [ '-9'   => 'negativa naŭ'           ],
+    [ '-9.0' => 'negativa naŭ komo nul'  ],
+    [   '.0' => 'komo nul'               ],
+    [  '0.'  => 'nul'                    ],
+    [  '0.0' => 'nul komo nul'           ],
+    [   '.9' => 'komo naŭ'               ],
+    [  '9'   => 'naŭ'                    ],
+    [  '9.'  => 'naŭ'                    ],
+    [ '+9'   => 'positiva naŭ'           ],
+    [ '+9.0' => 'positiva naŭ komo nul'  ],
+    [  '9.0' => 'naŭ komo nul'           ],
+
+    [  'inf' => 'senfineco'          ],
+    [ '+inf' => 'positiva senfineco' ],
+    [ '-inf' => 'negativa senfineco' ],
+    [  'NaN' => 'ne nombro'          ],
+);
+
+while (@tests) {
+    my ($num, $word) = @{ shift @tests };
+    is num2eo($num), $word, "$num is $word";
+}
+
+SKIP: {
+    skip 'bareword inf/NaN module not provided', 3;
+
+    my @skip_tests = (
+        [  inf => 'senfineco'          ],
+        [ -inf => 'negativa senfineco' ],
+        [  NaN => 'ne nombro'          ],
+    );
+
+    while (@skip_tests) {
+        my ($num, $word) = @{ shift @tests };
+        is num2eo($num), $word, "$num is $word";
+    }
+}
